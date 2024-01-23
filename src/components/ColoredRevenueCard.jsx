f74c1f4859736cd3d3cc0756400b9201f4de22dd
Aman Relan <aman.relan-ext@jci.com>
2024-01-23 20:28:30 +0530

    Added Cards and Views for the entire page

--- src/components/ColoredRevenueCard.jsx
+++ src/components/ColoredRevenueCard.jsx
@@ -1,10 +1,21 @@
 import RevenueCard from "./RevenueCard";
 
-const ColoredRevenueCard = () => {
+const ColoredRevenueCard = ({ title, orderCount, amount, paymentDate }) => {
   return (
-    <div>
-      <div className="bg-blue-400">
-        <RevenueCard title={"Next Payout"} orderCount={23} amount={"2312.23"} />
+    <div className="flex flex-col rounded-lg shadow-lg overflow-hidden bg-[#146EB4] hover:bg-[#0E4F82]">
+      <div>
+        <RevenueCard
+          title={title}
+          orderCount={orderCount}
+          amount={amount}
+          className={"text-white"}
+          orderCountTextColor={"text-white"}
+        />
+      </div>
+      <div className="px-6 py-2 bg-[#0E4F82] text-[#F2F2F2]">
+        <p>
+          Next Payment Date: <span>{paymentDate}</span>
+        </p>
       </div>
     </div>
   );
